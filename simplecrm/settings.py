360d2ace02407fa0c687a3dfefcbe0eb67c6a707
alexHooper <alexmohack@gmail.com>
2018-10-17 00:07:31 +0530

    configure django redis

--- simplecrm/settings.py
+++ simplecrm/settings.py
@@ -142,3 +142,15 @@ CORS_ORIGIN_WHITELIST = (
 
 # django-debug-toolbar settings
 INTERNAL_IPS = ['127.0.0.1']
+
+
+# django-redis settings
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
